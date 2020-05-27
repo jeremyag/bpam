04652346a067acaefca0b18aa0a4d0ccbdfbdec8
Jeremy Agcaoili <agcaoili.jeremy@gmail.com>
2020-05-27 19:54:49 +0800

    FIXED string to boolean error.

--- resources/js/components/layout/NavigationDrawer.vue
+++ resources/js/components/layout/NavigationDrawer.vue
@@ -6,6 +6,7 @@
         permanent
         absolute
         dark
+        app
     >
         <v-list-item class="px-2">
             <v-list-item-avatar>
@@ -26,63 +27,63 @@
         </v-list-item>
         <v-divider></v-divider>
         <v-list 
-            dense="true"
-            nav="true"
-            rounded="true">
-            <v-list-item link="true">
+            dense
+            nav
+            rounded>
+            <v-list-item link>
                 <v-list-item-icon>
                     <v-icon>home</v-icon>
                 </v-list-item-icon>
                 <v-list-item-title>Home</v-list-item-title>
             </v-list-item>   
-            <v-subheader inset="true">Management</v-subheader>
+            <v-subheader inset>Management</v-subheader>
             <v-list-item-group>
-                <v-list-item link="true">
+                <v-list-item link>
                     <v-list-item-icon>
                         <v-icon>description</v-icon>
                     </v-list-item-icon>
                     <v-list-item-title>Applications</v-list-item-title>
                 </v-list-item>
-                <v-list-item link="true">
+                <v-list-item link>
                     <v-list-item-icon>
                         <v-icon>business</v-icon>
                     </v-list-item-icon>
                     <v-list-item-title>Businesses</v-list-item-title>
                 </v-list-item>
-                <v-list-item link="true">
+                <v-list-item link>
                     <v-list-item-icon>
                         <v-icon>verified_user</v-icon>
                     </v-list-item-icon>
                     <v-list-item-title>Verifications</v-list-item-title>
                 </v-list-item>
-                <v-list-item link="true">
+                <v-list-item link>
                     <v-list-item-icon>
                         <v-icon>account_balance_wallet</v-icon>
                     </v-list-item-icon>
                     <v-list-item-title>Assessments</v-list-item-title>
                 </v-list-item>
-                <v-list-item link="true">
+                <v-list-item link>
                     <v-list-item-icon>
                         <v-icon>security</v-icon>
                     </v-list-item-icon>
                     <v-list-item-title>Fire Safety Assessments</v-list-item-title>
                 </v-list-item>
             </v-list-item-group>
-            <v-subheader inset="true">Administration</v-subheader>
+            <v-subheader inset>Administration</v-subheader>
             <v-list-item-group>
-                <v-list-item link="true">
+                <v-list-item link>
                     <v-list-item-icon>
                         <v-icon>supervised_user_circle</v-icon>
                     </v-list-item-icon>
                     <v-list-item-title>Users</v-list-item-title>
                 </v-list-item>
-                <v-list-item link="true">
+                <v-list-item link>
                     <v-list-item-icon>
                         <v-icon>perm_identity</v-icon>
                     </v-list-item-icon>
                     <v-list-item-title>Roles</v-list-item-title>
                 </v-list-item>
-                <v-list-item link="true">
+                <v-list-item link>
                     <v-list-item-icon>
                         <v-icon>how_to_reg</v-icon>
                     </v-list-item-icon>
