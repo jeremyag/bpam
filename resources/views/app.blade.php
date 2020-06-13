8089a274e13f032ca8a80c35c0e9ffe646717ebc
Jeremy <j.agcaoili@upteamco.com>
2020-06-13 17:42:17 +0800

    FIXED linking

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
 <head>
   <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Material+Icons" rel="stylesheet">
-  <link rel="stylesheet" href="{{asset('css/app.css')}}">
+  <link rel="stylesheet" href="/css/app.css">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
 </head>
 <body>
@@ -16,6 +16,6 @@
       </v-content>
     </v-app>
   </div>
-  <script src="{{asset('js/app.js')}}"></script>
+  <script src="/js/app.js"></script>
 </body>
 </html>
