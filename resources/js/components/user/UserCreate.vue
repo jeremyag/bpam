45c70308651ef45967b6f133ec736c56a0b338ca
Jeremy <j.agcaoili@upteamco.com>
2020-06-13 15:44:21 +0800

    CREATED UserCreate component.

---
+++ resources/js/components/user/UserCreate.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <v-breadcrumbs :items="items">
+            <template v-slot:divider>
+                <v-icon>chevron_right</v-icon>
+            </template>
+        </v-breadcrumbs>
+        <app-user-form></app-user-form>
+    </div>
+</template>
+
+<script>
+import UserForm from "./UserForm";
+export default {
+    data() {
+        return {
+            items: [
+                {
+                    text: "Users",
+                    to: "/users",
+                    link: true
+                },
+                {
+                    text: "New",
+                    disabled: true
+                }
+            ]
+        };
+    },
+    components: {
+        'AppUserForm': UserForm
+    }
+}
+</script>
