d18a68fdf970cefb02ec636c4a0464126d906e08
Jeremy <j.agcaoili@upteamco.com>
2020-06-13 15:45:11 +0800

    ADDED user/new in router-link

--- resources/js/plugins/route.js
+++ resources/js/plugins/route.js
@@ -4,13 +4,25 @@ import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 import Login from '../components/user/Login.vue';
 import Home from '../components/home/Home.vue';
+
+
+/**
+ * Users Module
+ */
 import UserList from '../components/user/UserList.vue';
+import User from '../components/user/User.vue';
+import UserCreate from '../components/user/UserCreate.vue';
 
 const options = {
     routes: [
         {path: '/login', component: Login},
         {path: '/home', component: Home},
-        {path: '/user', component: UserList}
+        {path: '/users', component: UserList},
+        {path: '/user', component: User,
+            children: [
+                {path: 'new', component: UserCreate}
+            ]
+        }
     ]
 };
 export default new VueRouter(options);
