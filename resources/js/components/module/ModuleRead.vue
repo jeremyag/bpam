7c16d34f3a860cc98e50cc79cbbbd3978478fc2b
Jeremy <j.agcaoili@upteamco.com>
2020-06-14 16:47:36 +0800

    ADDED module read mode.

---
+++ resources/js/components/module/ModuleRead.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <v-breadcrumbs :items="items">
+            <template v-slot:divider>
+                <v-icon>chevron_right</v-icon>
+            </template>
+        </v-breadcrumbs>
+        <bpam-module-form readonly :title="'Module'"></bpam-module-form>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            items: [
+                {
+                    text: "Modules",
+                    to: "/modules",
+                    link: true
+                },
+                {
+                    text: this.$route.params.id,
+                    disabled: true
+                }
+            ]
+        };
+    }
+}
+</script>
